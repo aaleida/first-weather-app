d17198cc192387bf8187ed37a7086c43dae5457c
aaleida <alena.aleida@gmail.com>
2024-12-18 01:18:58 +0100

    updated html and js for current icon

--- src/script.js
+++ src/script.js
@@ -5,14 +5,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-
-
-
+  let iconElement = document.querySelector("#icon");
+  
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = temperature;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url} class="weather-app-icon" /`;
 }
 
 function search(event) {
