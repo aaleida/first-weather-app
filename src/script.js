186b9d10efd87ed673d527b613ae8d58f018bf19
aaleida <alena.aleida@gmail.com>
2024-12-19 19:30:04 +0100

    updated js icon fixed

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = temperature;
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" /`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function search(event) {
